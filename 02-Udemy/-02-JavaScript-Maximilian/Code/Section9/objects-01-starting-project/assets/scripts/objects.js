18a8bebadcefdb286737fd946675df5de8933828
shawky_53 <mostafashawky100000@gmail.com>
2023-03-07 20:59:19 +0200

    objects

---
+++ 02-Udemy/-02-JavaScript-Maximilian/Code/Section9/objects-01-starting-project/assets/scripts/objects.js
@@ -0,0 +1,14 @@
+const cv = {
+  fname: "mostafa",
+  lname: "shawky",
+  age: 21,
+  address: {
+    city: "El Senbellawein",
+    country: "egypt",
+  },
+  skills: ["html", "css", "js", "react", "nodejs"],
+  // greet: function () {
+  //   alert("hello world");
+  // },
+};
+// cv.greet();
