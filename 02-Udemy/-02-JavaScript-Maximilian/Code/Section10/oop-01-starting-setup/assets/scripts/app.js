3cc304a97e221ac382e86a24ccfd131abb1aaca7
shawky_53 <mostafashawky100000@gmail.com>
2023-03-30 11:24:35 +0200

    7. Using & Connecting Multiple Classes

--- 02-Udemy/-02-JavaScript-Maximilian/Code/Section10/oop-01-starting-setup/assets/scripts/app.js
+++ 02-Udemy/-02-JavaScript-Maximilian/Code/Section10/oop-01-starting-setup/assets/scripts/app.js
@@ -1,5 +1,5 @@
 class Product {
-  // title = "DEFAULT";
+  // title = 'DEFAULT';
   // imageUrl;
   // description;
   // price;
@@ -11,36 +11,60 @@ class Product {
     this.price = price;
   }
 }
+
+class ProductItem {
+  constructor(product) {
+    this.product = product;
+  }
+
+  render() {
+    const prodEl = document.createElement("li");
+    prodEl.className = "product-item";
+    prodEl.innerHTML = `
+        <div>
+          <img src="${this.product.imageUrl}" alt="${this.product.title}" >
+          <div class="product-item__content">
+            <h2>${this.product.title}</h2>
+            <h3>\$${this.product.price}</h3>
+            <p>${this.product.description}</p>
+            <button>Add to Cart</button>
+          </div>
+        </div>
+      `;
+    return prodEl;
+  }
+}
+
 class ProductList {
   products = [
-    new Product("Product 1", "https://picsum.photos/200/300", "product 1", 100),
-    new Product("Product 2", "https://picsum.photos/200/300", "Product 2", 200),
-    new Product("Product 3", "https://picsum.photos/200/300", "Product 3", 300),
-    new Product("Product 4", "https://picsum.photos/200/300", "product 4", 400),
+    new Product(
+      "A Pillow",
+      "https://www.maxpixel.net/static/photo/2x/Soft-Pillow-Green-Decoration-Deco-Snuggle-1241878.jpg",
+      "A soft pillow!",
+      19.99
+    ),
+    new Product(
+      "A Carpet",
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/7/71/Ardabil_Carpet.jpg/397px-Ardabil_Carpet.jpg",
+      "A carpet which you might like - or not.",
+      89.99
+    ),
   ];
-}
-const productList = {
+
+  constructor() {}
+
   render() {
     const renderHook = document.getElementById("app");
     const prodList = document.createElement("ul");
     prodList.className = "product-list";
     for (const prod of this.products) {
-      const prodEl = document.createElement("li");
-      prodEl.className = "product-item";
-      prodEl.innerHTML = `
-        <div>
-          <img src="${prod.imageUrl}" alt="${prod.title}">
-          <div class="product-item__content">
-            <h2>${prod.title}</h2>
-            <h3>\$${prod.price}</h3>
-            <p>${prod.description}</p>
-            <button>Add to Cart</button>
-          </div>
-        </div>
-      `;
+      const productItem = new ProductItem(prod);
+      const prodEl = productItem.render();
       prodList.append(prodEl);
     }
-  },
-};
+    renderHook.append(prodList);
+  }
+}
 
+const productList = new ProductList();
 productList.render();
