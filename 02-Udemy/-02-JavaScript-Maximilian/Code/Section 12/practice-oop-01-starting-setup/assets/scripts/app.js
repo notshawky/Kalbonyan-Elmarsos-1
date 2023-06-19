62d4255aeffede861fd09f6d42fea4f9d6b5170a
shawky_53 <mostafashawky100000@gmail.com>
2023-06-19 13:59:32 +0300

    create project list

---
+++ 02-Udemy/-02-JavaScript-Maximilian/Code/Section 12/practice-oop-01-starting-setup/assets/scripts/app.js
@@ -0,0 +1,19 @@
+class Tooltip {}
+
+class ProjectItem {}
+
+class ProjectList {
+  constructor(type) {
+    const projectItem = document.querySelectorAll(`#${type}-projects li`);
+    console.log(projectItem);
+  }
+}
+
+class App {
+  static init() {
+    const activeProjectsList = new ProjectList("active");
+    const finishedProjectsList = new ProjectList("finished");
+  }
+}
+
+App.init();
